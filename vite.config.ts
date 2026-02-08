b0131a0a3c172d16648401edd6e4ef0d021fce3c
Bloodreaper80085 <155052860+Bloodreaper80085@users.noreply.github.com>
2026-02-08 16:13:35 -0500

    Update vite.config.ts with the latest configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,28 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
-});
+export default defineConfig(
+  process.env.ELECTRON
+    ? {
+        plugins: [react()],
+        optimizeDeps: {
+          exclude: ['lucide-react'],
+        },
+        build: {
+          outDir: 'dist',
+          rollupOptions: {
+            input: path.resolve(__dirname, 'index.html'),
+          },
+        },
+        server: {
+          middlewareMode: true,
+        },
+      }
+    : {
+        plugins: [react()],
+        optimizeDeps: {
+          exclude: ['lucide-react'],
+        },
+      }
+);
